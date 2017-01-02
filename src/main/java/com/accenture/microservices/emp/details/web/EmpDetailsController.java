8217cc05f7e8c2a8e9857b6e8a3f67267f074473
Agarwal <mahima.agarwal@accenture.com>
2017-01-02 09:41:40 +0530

    updated the end points and removed validate end point

--- src/main/java/com/accenture/microservices/emp/details/web/EmpDetailsController.java
+++ src/main/java/com/accenture/microservices/emp/details/web/EmpDetailsController.java
@@ -4,7 +4,6 @@
 package com.accenture.microservices.emp.details.web;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -35,7 +34,7 @@ public class EmpDetailsController {
 	@Autowired
 	EmployeeRepository repository;
 	
-	@RequestMapping(value = "/employee/details/{empId}",method = RequestMethod.GET)
+	@RequestMapping(value = "/employees/{empId}",method = RequestMethod.GET)
 	public EmployeeDetails getEmployeeDetails(@PathVariable ("empId") long id) throws Exception{
 		log.info("EmpDetailsController path variable: Employee ID "+id);
 		EmployeeDetails emplDetails = empDetailsService.getEmployeeDetails(id);
@@ -45,16 +44,16 @@ public class EmpDetailsController {
 		return emplDetails;
 	}
 	
-	@RequestMapping(value = "/employee/validate/{empId}",method = RequestMethod.GET)
-	public boolean validateEmployee(@PathVariable ("empId") long id) throws Exception{
-		log.info("EmpDetailsController path variable: Employee ID "+id);
-		boolean result = false;
-		result = empDetailsService.checkEmployeeExists(id);
-		log.info("EmpDetailsController validateEmployee result" + result);
-		return result;
-	}
+//	@RequestMapping(value = "/employees/validate/{empId}",method = RequestMethod.GET)
+//	public boolean validateEmployee(@PathVariable ("empId") long id) throws Exception{
+//		log.info("EmpDetailsController path variable: Employee ID "+id);
+//		boolean result = false;
+//		result = empDetailsService.checkEmployeeExists(id);
+//		log.info("EmpDetailsController validateEmployee result" + result);
+//		return result;
+//	}
 	
-	@RequestMapping(value = "/employee/list",method = RequestMethod.GET)
+	@RequestMapping(value = "/employees",method = RequestMethod.GET)
 	public List<EmployeeDetails> getAllEmployees() throws Exception{
 		log.info("EmpDetailsController getAllEmployees ");
 		List<EmployeeDetails> emplList = new ArrayList<EmployeeDetails>();
@@ -66,7 +65,7 @@ public class EmpDetailsController {
 		return emplList;
 	}
 	
-	@RequestMapping(value = "/employee/delete/{empId}",method = RequestMethod.GET)
+	@RequestMapping(value = "/employees/{empId}",method = RequestMethod.DELETE)
 	public String deleteEmployee(@PathVariable ("empId") long id) throws Exception{
 		log.info("EmpDetailsController deleteEmployee: Employee ID "+id);
 		String result = "false";
