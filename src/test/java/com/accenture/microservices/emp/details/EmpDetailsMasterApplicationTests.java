25052cabf238b307f293564a183af72f9794154c
unknown <haritha.devarajulu@accenture.com>
2017-04-01 14:59:51 +0530

    IntegrationTests

--- src/test/java/com/accenture/microservices/emp/details/EmpDetailsMasterApplicationTests.java
+++ src/test/java/com/accenture/microservices/emp/details/EmpDetailsMasterApplicationTests.java
@@ -2,11 +2,9 @@ package com.accenture.microservices.emp.details;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.nio.charset.Charset;
@@ -22,11 +20,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -37,21 +35,25 @@ import com.accenture.lari.employees.domain.EmployeeDetails;
 @SpringBootTest (classes = EmpDetailsMasterApplication.class)
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-public class EmpDetailsMasterApplicationTests {
+public class EmpDetailsMasterApplicationTests{
 
 public static final Logger log = LoggerFactory.getLogger(EmpDetailsMasterApplicationTests.class);
+
+/*@Autowired
+private SetterMessage message = null;*/
 	
 	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
             MediaType.APPLICATION_JSON.getSubtype(),
             Charset.forName("utf8"));
 	
-
 	private MockMvc mockMvc;
     
-//	private HttpMessageConverter mappingJackson2HttpMessageConverter;
+    //  private HttpMessageConverter mappingJackson2HttpMessageConverter;
     
 	@Autowired
     private WebApplicationContext webApplicationContext;
+	
+	
     
     @Before
     public void setup() throws Exception {
@@ -70,12 +72,79 @@ public static final Logger log = LoggerFactory.getLogger(EmpDetailsMasterApplica
 	@Test
 	public void getEmployeeDetails() throws Exception {
 		List<EmployeeDetails> allEmployeeRefList = createEmployeeObjectList();
-		MvcResult allEmployeesList = mockMvc.perform(get("/employees/1")).andDo(print()).andReturn();
+		MvcResult allEmployeesList = mockMvc.perform(get("/employees")).andDo(print()).andReturn();
 		Collection<EmployeeDetails> newComapreObject=new ArrayList<EmployeeDetails>();
 		newComapreObject.addAll(allEmployeeRefList);
+		log.info("All employees list");
 		assertThat(newComapreObject.equals(allEmployeesList));
 	}
 	
+	/**
+     * 
+     * @throws Exception
+     * @Description: positive test case- Unit test with parameter employeeId
+     * @param: employeeId
+     * @return: EmployeeDetails
+     * @expected result: EmployeeDetails document
+     */
+     
+	
+	@Test
+	public  void getEmployee() throws Exception {
+		List<EmployeeDetails> Employee = createEmployeeObjectList();
+		MvcResult allEmployeesList = mockMvc.perform(get("/employees/1233")).andDo(print()).andReturn();
+		Collection<EmployeeDetails> newComapreObject=new ArrayList<EmployeeDetails>();
+		newComapreObject.addAll(Employee);
+		assertThat(newComapreObject.equals(allEmployeesList));
+		log.info("employee number:1233");
+	}
+	
+	/**
+	@Test
+	public void testMessage()
+	{
+		assertNotNull("Constructor message instance is null ", message);
+		String msg = ((Throwable) message).getMessage();
+		assertNotNull("Message is null.",msg);
+		String  expectedMessage = "Employee details should not be null";
+		assertEquals("Message shuld be '"+expectedMessage + "'.", expectedMessage,msg);
+		log.info("message='{}'",msg); 
+	}*/
+	
+	@Test
+    public void EMployeeNotFound() throws Exception {
+		MvcResult allEmployeesList = mockMvc.perform(get("/employees")).andDo(print()).andReturn();
+		log.info("Employee not found");
+               
+    }
+	
+	
+	@Test
+	public void test_get_all_success() throws Exception {
+	   	    mockMvc.perform(get("/employees"))
+	            .andExpect(status().isOk())
+	            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
+	              	}
+
+	
+	/**@Test
+	public void test_create_employees_fail_404_not_found() throws Exception {
+	    
+	    mockMvc.perform(get("/employees"))
+	    .andExpect(status().isNotFound())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));  
+	    log.info("Bad Exception");
+	   	}*/
+	
+	
+	
+
+	/**
+	public static void fail(String message) {
+		throw new AssertionError(message);
+	}*/
+
+		
 	/**
 	 * 
 	 * @return Employee lIst generator method
@@ -101,30 +170,34 @@ public static final Logger log = LoggerFactory.getLogger(EmpDetailsMasterApplica
 	}
 
 		
-	/**//**
+	/**
 	 * 
 	 * @throws Exception
 	 * @Description: positive test case- Unit Test with out employeeId
 	 * @param: null
      * @return: EmployeeDetails arraylist
      * @expected result: arraylist of EmployeeDetails  document 
-	 *//*
+	 */
+	 
+	 
 	@Test
 	public void getAllEmployees() throws Exception {
 		log.info(" Inside getAllEmployees");
-//		List<EmployeeDetails> emplList = new ArrayList<EmployeeDetails>();
+	List<EmployeeDetails> emplList = new ArrayList<EmployeeDetails>();
 		mockMvc.perform(get("/employees")).andExpect(status().isOk()).andDo(print()).andExpect(content().contentType(contentType));
 	}
 	
-	  *//**
+	/**
      * 
      * @throws Exception
      * @Description: negative test case- Unit test with parameter employeeId
      * @param: employeeId
      * @return: null
      * @expected result: 404 
-     * 
-     *//*	
+     *
+     
+     
+	
 	@Test
 	public void getEmployeeDetailsURLCheck() throws Exception {
 		log.info(" Inside getEmployeeDetailsURLCheck");
@@ -157,6 +230,8 @@ public static final Logger log = LoggerFactory.getLogger(EmpDetailsMasterApplica
      * @expected result: false
      * 
      *//*
+     
+     
 	@Test
 	public void deleteEmployee() throws Exception {
 		MvcResult result = mockMvc.perform(delete("/employees/1233")).andDo(print()).andReturn();
